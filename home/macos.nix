c36622d5138c197318d572063a690875c83fd382
viraj-sh <virajs45@proton.me>
2025-11-30 16:58:11 +0530

    refactor: streamline home configuration in macos.nix

--- home/macos.nix
+++ home/macos.nix
@@ -7,13 +7,11 @@
 }: let
 in {
   imports = [];
-  home = {
-    username = "virajs";
-    homeDirectory = "/Users/virajs";
-    stateVersion = "25.05";
-    file = {};
-    sessionVariables = {};
-  };
+  username = "virajs";
+  homeDirectory = builtins.getEnv "HOME";
+  stateVersion = "25.05";
+  file = {};
+  sessionVariables = {};
   programs.home-manager.enable = true;
   programs.nix-index.enable = true;
   programs.nix-index.enableBashIntegration = true;
