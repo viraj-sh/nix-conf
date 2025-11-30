5ee92710de53ed8bea2cf9d7136061468fe8a6dd
virajsh <virajs45@proton.me>
2025-11-30 18:08:05 +0530

    add gcm

--- home/macos.nix
+++ home/macos.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   imports = [./modules/git];
+  home.packages = with pkgs; [
+    git-credential-manager
+  ];
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 }
