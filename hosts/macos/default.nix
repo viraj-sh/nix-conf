3bd60a0461e3a30d8688d75e17220c6ff94985af
virajsh <virajs45@proton.me>
2025-12-01 22:07:55 +0530

    feat: stylix config

--- hosts/macos/default.nix
+++ hosts/macos/default.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
+    imports = [
+    ./stylix.nix
+  ];
   users.users = {
     virajs = {
       uid = 501; # macOS user ID
@@ -18,4 +21,21 @@
 
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
+  system.primaryUser = "virajs";
+  system.defaults.dock = {
+    autohide = false;
+    show-recents = false;
+    tilesize = 48;
+    mineffect = "genie";
+    orientation = "left";
+    minimize-to-application = true;
+    mru-spaces = false;
+    persistent-apps = [
+      "/System/Applications/Home.app/"
+      "/nix/store/aj1cv2ic5ziqsm72m95drzi9qq892q8z-kitty-0.42.1/Applications/kitty.app"
+      "/nix/store/5l4gg55zzz7nldp9cd2jp2jzvdpdpdwq-brave-1.84.141/Applications/Brave Browser.app"
+      "/nix/store/fkxwb5s1p1c0vrxam3q10l2pm0ln2dvy-vscode-1.105.0/Applications/Visual Studio Code.app"
+    ];
+};
+
 }
