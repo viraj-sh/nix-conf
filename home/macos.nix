7afb205d42815943c214bbf0327b81e0958d3624
virajsh <virajs45@proton.me>
2025-12-09 17:26:53 +0530

    hm module: alacritty terminal

--- home/macos.nix
+++ home/macos.nix
@@ -2,10 +2,12 @@
   imports = [
     ./modules/git
     ./modules/kitty
+    ./modules/alacritty
     ./modules/fastfetch
     ./modules/discord
     ./modules/vscode
     ./modules/shell
+
   ];
   home.stateVersion = "25.11";
   programs.home-manager.enable = true;
