89344116c108924d63fa9f2d8e01d6f400090c4d
viraj-sh <virajs45@proton.me>
2025-12-10 11:15:32 +0530

    feat: add neovim plugin configuration and key mappings

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -12,5 +12,37 @@
     enable = true;
     defaultEditor = true;
     vimdiffAlias = true;
+    plugins = {
+      telescope.enable = true;
+      treesitter.enable = true;
+      luasnip.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          lua-ls.enable = true;
+        };
+      };
+      nvim-cmp = {
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+      };
+    };
+    options = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+    globals.mapleader = " ";
+    keymaps = [
+      {
+        action = "<cmd>Telescope live_grep<CR>";
+        key = "<leader>g";
+      }
+    ];
   };
 }
