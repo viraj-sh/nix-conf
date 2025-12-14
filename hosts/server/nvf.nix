0353414fc6508665491e4b1754dec0f729a39479
viraj-sh <virajs45@proton.me>
2025-12-14 14:25:14 +0530

    add home manager module nvf

--- hosts/server/nvf.nix
+++ hosts/server/nvf.nix
@@ -10,10 +10,31 @@
   programs.nvf = {
     enable = true;
     settings = {
-      vim.theme.enable = true;
-      vim.theme.name = "gruvbox";
-      vim.theme.style = "dark";
-      vim.languages.nix.enable = true;
+      vim = {
+        theme = {
+          enable = true;
+          # name = "gruvbox";
+          style = "dark";
+        };
+        languages = {
+          nix.enable = true;
+          java.enable = true;
+          python.enable = true;
+          clang.enable = true;
+          html.enable = true;
+          css.enable = true;
+          enableLSP = true;
+          enableTreesitter = true;
+          };
+        assistant.copilot.cmp.enable = true;
+        assistant.copilot.enable = true;
+  
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+      };
+      
+
     };
-  }
+  };
 }
