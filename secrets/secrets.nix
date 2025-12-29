3111013af3611bddc06e5e9e0608c2bf2804f817
viraj-sh <virajs45@proton.me>
2025-12-30 03:08:29 +0530

    n8n secrets using agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,5 @@
 let
   virajs-server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWHDW+JNMHUPMKDnXDuTiSuX1jKi35zuILluPUIuiDB";
-in
-{
-  "secret1.age".publicKeys = [virajs-server];
+in {
+  "n8n.age".publicKeys = [ virajs-server ];
 }
