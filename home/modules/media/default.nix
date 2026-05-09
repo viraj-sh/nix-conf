f2bd9d98c523dbfc90af05201a3c544d1e93c0c0
viraj-sh <virajs45@proton.me>
2026-05-10 01:04:16 +0530

    add obs hm options

--- home/modules/media/default.nix
+++ home/modules/media/default.nix
@@ -8,10 +8,17 @@
   imports = [
   ];
   home.packages = with pkgs; [
-    obs-studio
-    obs-studio-plugins.obs-teleport
+    # obs-studio
+    # obs-studio-plugins.obs-teleport
     wf-recorder
     spotdl
 
   ];
+  programs.obs-studio = {
+    package = pkgs.obs-studio;
+    enable = true;
+    plugins = [ pkgs.obs-studio-plugins.obs-teleport ];
+
+  };
+
 }
