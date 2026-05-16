265ac629b7215a1a3ae48e7062dae1897a415bd5
viraj-sh <virajs45@proton.me>
2026-05-16 16:25:07 +0000

    initial commit nixos laptop

--- home/laptop.nix
+++ home/laptop.nix
@@ -1,1 +1,11 @@
-{}
+{
+pkgs,
+lib,
+...
+}: {
+imports = [./modules/git];
+home.username = "virajs-laptop";
+home.homeDirectory = lib.mkForce "/home/virajs-laptop";
+home.stateVersion = "25.11";
+programs.home-manager.enable=true;
+}
