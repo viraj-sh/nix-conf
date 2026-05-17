038351cb57efb524b84bc243ff8d69e09a18f202
viraj-sh <virajs45@proton.me>
2026-05-17 15:57:45 +0530

    feat: comment out sddm package setting in KDE configuration

--- nixos/modules/desktop/kde.nix
+++ nixos/modules/desktop/kde.nix
@@ -15,6 +15,6 @@
   services.desktopManager.plasma6.enable = true;
   services.desktopManager.plasma6.enableQt5Integration = true;
   services.desktopManager.plasma6.notoPackage = pkgs.noto-fonts;
-  services.displayManager.sddm.package = pkgs.kdePackages.sddm;
+  # services.displayManager.sddm.package = pkgs.kdePackages.sddm;
   services.displayManager.sddm.enable = true;
 }
